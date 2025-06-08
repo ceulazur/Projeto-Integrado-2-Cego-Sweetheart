540f2dbda15b8619011d059897cbe13150ba41b5
Paulo Victor <victorcostaferreira22@gmail.com>
2025-06-08 00:41:14 -0300

    tamanho da imagem consertada

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Hero: React.FC = () => {
   return (
-    <section className="flex flex-col relative aspect-[2.08] w-[389px] max-w-full text-[15px] text-white font-bold whitespace-nowrap mt-2.5 pt-[85px] pb-12 px-0.5">
+    <section className="flex flex-col relative aspect-[2.08] w-[425px] max-w-full text-[15px] text-white font-bold whitespace-nowrap mt-2.5 pt-[85px] pb-12 px-0.5">
       <img
         src="https://cdn.builder.io/api/v1/image/assets/c9e61df7bfe543a0b7e24feda3172117/ddc1f9ea31b7878961ee5f50ef9bc293b936e634?placeholderIfAbsent=true"
         alt="Hero background"
