336a414ac92e47be2d290ead049feca59947d856
Paulo Victor <victorcostaferreira22@gmail.com>
2025-06-08 02:02:15 -0300

    resolvido o gap vertical do header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -21,13 +21,8 @@ export const Header: React.FC = () => {
 
           {/* Menu mobile e Menu */}
           <div className="flex items-center space-x-4">
-            <button className="bg-blue-900 p-1 rounded">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-white" viewBox="0 0 20 20" fill="currentColor">
-                <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" />
-              </svg>
-            </button>
-            <button className="lg:hidden">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <button>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-blue-900" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
               </svg>
             </button>
